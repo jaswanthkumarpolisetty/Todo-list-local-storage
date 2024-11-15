4cf55766a36c2b8bd883b50c5df887768c516f09
Jaswanth kumar polisetty <88545021+jaswanthkumarpolisetty@users.noreply.github.com>
2024-11-15 11:29:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-const todoInput = document.getElementById("todo-input")
-const addTaskButton = document.getElementById("add-task-btn")
-const todoList = document.getElementById("todo-list");
+document.addEventListener('DOMContentLoaded', () => {
+    const todoInput = document.getElementById("todo-input")
+    const addtasksButton = document.getElementById("add-tasks-btn")
+    const todoList = document.getElementById("todo-list");
 
-let tasks = localStorage.getItem('tasks') || [];
-addTaskButton.addEventListener('click', ()=>{
-    const taskTest = todoInput.value.trim()
-    if(taskTest === "") return ;
-    const newTask = {
-        id:Date.now(),
-        text: taskTest,
-        completed: false 
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach(task => {
+        rendertasks(task)
+    });
+
+    addtasksButton.addEventListener('click', () => {
+        const tasksTest = todoInput.value.trim()
+        if (tasksTest === "") return;
+        const newtasks = {
+            id: Date.now(),
+            text: tasksTest,
+            completed: false
+        }
+        tasks = [...tasks, newtasks];
+        rendertasks(newtasks)
+        savetasks();
+        todoInput.value = "";
+    });
+
+    function rendertasks(task) {
+        const li =document.createElement('li')
+        li.setAttribute('data-id',task.id)
+        if(task.completed)li.classList.add("completed")
+        li.innerHTML=`<div>${task.text}</div>
+        <button>delete</button>`
+        li.addEventListener('click', (e)=>{
+            if(e.target.tagName === 'BUTTON') return;
+            li.classList.toggle("completed")
+            task.completed=!task.completed;
+            savetasks();
+        })
+        li.querySelector('button').addEventListener('click',(e)=>{
+            e.stopPropagation()  // prevent toggle for firing
+            tasks=tasks.filter((t)=> t.id!==task.id);
+            li.remove();
+            savetasks();
+        })
+        todoList.appendChild(li);
     }
-    tasks=[...tasks,newTask];
-    saveTasks();
-    console.log(task);
-    todoInput.value="";
-});
 
-function saveTasks(){
-    localStorage.setItem('tasks',JSON.stringify(tasks));
-}
+    function savetasks() {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+})
